3f31c1e6067fbcda277521857d75a4eae6c2c0fb
Gabe Hassler <gabeh12345@gmail.com>
2024-01-28 17:56:20 -0800

    connect to wifi

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-import utils.sensors as sensors
+import network
+from time import sleep
 
-sensors.test_sensors()
+# read 'secrets.json' file
+
+import json
+with open('secrets.json') as f:
+    secrets = json.load(f)['wifi']
+
+
+
+
+
+def connect(max_attempts=10):
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(secrets['ssid'], secrets['password'])
+    attempts = 1
+    while wlan.isconnected() == False and attempts < max_attempts:
+        print('Connecting to network... attempt', attempts)
+        attempts += 1
+        sleep(1)
+    print("Connected to WiFi")
+
+
+connect()
+
